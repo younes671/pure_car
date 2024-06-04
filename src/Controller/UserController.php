be27aa60d3d4191340c5e28d4ffc7c9266efdbed
Younesse AL AMRI <younes671@live.fr>
2024-06-04 20:27:26 +0200

    ajout edit mot de passe , supprimer compte utilisateur , access control

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,9 +6,12 @@ use App\Entity\User;
 use App\Entity\Reservation;
 use App\Repository\UserRepository;
 use App\Repository\ReservationRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class UserController extends AbstractController
 {
@@ -31,14 +34,38 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/user/profil/{idClient}', name: 'profil_user')]
-    public function profilUser(User $idClient, ReservationRepository $reservationRepository): Response
+    #[Route('/user/delete/{id}', name: 'delete_user')]
+    public function supprimerUtilisateur(User $user, UserRepository $userRepository, EntityManagerInterface $entityManager): RedirectResponse
     {
+        
+        // supprimer toutes les réservations de l'utilisateur
+        foreach ($user->getUserReservation() as $reservation) {
+            $entityManager->remove($reservation);
+        }
+        //supprimer l'utilisateur
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Votre compte a été supprimé avec succès');
+        return $this->redirectToRoute('app_register');
+    }
+
+    #[Route('/user/profil/{idClient}', name: 'profil_user')]
+    public function profilUser(User $idClient, Security $security, ReservationRepository $reservationRepository): Response
+    {
+        if ($security->isGranted('ROLE_ADMIN') || $security->isGranted('ROLE_USER'))
+        {
         $reservation = $reservationRepository->findBy(['user' => $idClient]);
         // var_dump($reservation); exit;
         return $this->render('user/profil.html.twig', [
             'reservations' => $reservation,
         ]);
+        }else{
+
+            $this->addFlash('danger', 'Vous n\'avez pas accès à cette page, veuillez vous connectez.');
+            return $this->redirectToRoute('app_login');
+        }
     }
+
 
 }
